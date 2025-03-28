67e5add27002d87d704765b0171568ee3793fab5
spripon <priponsorin@yahoo.com>
2025-03-28 21:30:48 +0100

    Delete sift_perspective/sift_detect.py

--- sift_perspective/sift_detect.py
+++
@@ -1,8 +0,0 @@
-
-import cv2
-
-def sift_detect(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    sift = cv2.SIFT_create()
-    keypoints, descriptors = sift.detectAndCompute(gray, None)
-    return keypoints, descriptors
