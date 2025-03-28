204b4fd9b44f845efd8d5c25f0454851186a432c
spripon <priponsorin@yahoo.com>
2025-03-28 20:48:07 +0100

    Add files via upload

--- sift_perspective/sift_detect.py
+++ sift_perspective/sift_detect.py
@@ -1,1 +1,8 @@
 
+import cv2
+
+def sift_detect(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    sift = cv2.SIFT_create()
+    keypoints, descriptors = sift.detectAndCompute(gray, None)
+    return keypoints, descriptors
